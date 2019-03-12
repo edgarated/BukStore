eff71a8a05f48687ec099307599a1c1e0e9a6bce
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-12 19:36:29 +0100

    added the search function

--- bukstore.js
+++ bukstore.js
@@ -28,7 +28,7 @@ bookName.textContent = value
 deleteBtn.classList.add("delete");
 bookName.classList.add("name");
 
-// append to DOM
+// // append to DOM
 li.appendChild(bookName);
 li.appendChild(deleteBtn);
 list.appendChild(li);
@@ -48,28 +48,22 @@ list.style.display = "none"
 
 });
 
-//The search function
+//Search Function
 
 const searchBar = document.forms["search-books"].querySelector("input");
-searchBar.addEventListener("keyup" , function(e){
-
+searchBar.addEventListener("keyup", function(e){
 const term = e.target.value.toLowerCase();
-const  books = list.getElementsByTagName("li");
+const books = list.getElementsByTagName("li");
 Array.from(books).forEach(function(book){
-
-  const title = book.firstElementChild.textContent;
-  if(title.toLowerCase().indexOf(term)!= -1){
-
-    book.style.display = "block";
-  }else{
-    book.style.display = "none";
-  }
-
-})
+const title = book.firstElementChild.textContent;
+if(title.toLocaleLowerCase().indexOf(term)!= -1){
+  book.style.display = "block";
+}else{
+  book.style.display = "none";
+}
 
 });
 
-
-
+})
 
 
