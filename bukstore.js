494527b6bae7c8b8d239e8b0c16e40d267095513
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-14 20:45:51 +0100

    added the contact and about us tabs

--- bukstore.js
+++ bukstore.js
@@ -75,11 +75,11 @@ tabs.addEventListener("click", function(e){
   if(e.target.tagName =="LI"){
     const targetPanel =document.querySelector(e.target.dataset.target);
     panels.forEach(function(panel){
-   if (panel == targetPanel){
-     panel.classList.add("active");
-   }else{
-     panel.classList.remove("active");
-   }
+  if (panel == targetPanel){
+    panel.classList.add("active");
+  }else{
+    panel.classList.remove("active");
+  }
 
     })
   }
